df6b03387cafb573e75ed8066a1c21cc81a939e7
Amar Jeet Singh <amarjesi@mx1.ibm.com>
2017-09-29 20:52:03 -0500

    added router module

--- hello-world/src/app/github-followers/github-followers.component.ts
+++ hello-world/src/app/github-followers/github-followers.component.ts
@@ -1,25 +1,38 @@
 import { GithubFollowerService } from './../services/github-follower/github-follower.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/combineLatest';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/map';
 
 @Component({
-  selector: 'app-github-followers',
-  templateUrl: './github-followers.component.html',
-  styleUrls: ['./github-followers.component.css']
+    selector: 'app-github-followers',
+    templateUrl: './github-followers.component.html',
+    styleUrls: ['./github-followers.component.css']
 })
 export class GithubFollowersComponent implements OnInit {
-  followersArray: any[];
-  private url = 'https://api.github.com/users/mosh-hamedani/followers';
-  constructor(private githubService: GithubFollowerService) { }
 
-  ngOnInit() {
+    followersArray: any[];
+    private url = 'https://api.github.com/users/mosh-hamedani/followers';
 
-    this.githubService.getAllFollowers(this.url)
-    .subscribe(
-      response => {
-        this.followersArray = response;
-        //console.log(response);
-      }
-    );
-  }
+    constructor(private githubService: GithubFollowerService, private route: ActivatedRoute) { }
+
+    ngOnInit() {
+
+        Observable.combineLatest([
+            this.route.paramMap,
+            this.route.queryParamMap
+        ])
+            .switchMap(combined => {
+                console.log(combined);
+                return this.githubService.getAllFollowers(this.url);      
+
+            })
+            .subscribe(followers => {this.followersArray = followers; });
+            
+  
+
+    }
 
 }
--- hello-world/src/app/github-profile/github-profile.component.ts
+++ hello-world/src/app/github-profile/github-profile.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router} from '@angular/router';
 
 @Component({
-  selector: 'app-github-profile',
-  templateUrl: './github-profile.component.html',
-  styleUrls: ['./github-profile.component.css']
+    selector: 'app-github-profile',
+    templateUrl: './github-profile.component.html',
+    styleUrls: ['./github-profile.component.css']
 })
 export class GithubProfileComponent implements OnInit {
 
-  constructor() { }
+    private username: string;
+    private id: number;
 
-  ngOnInit() {
-  }
+    constructor(private activatedRoute: ActivatedRoute, private router: Router ) { }
+
+     ngOnInit(){ 
+
+    console.log('GithubProfileComponent onInit');
+      
+    this.activatedRoute.paramMap.subscribe(
+
+    params => {
+        this.username = params.get('username');
+        this.id = +params.get('id');
+    }
+); 
+ }
+
+submit(){
+        
+    this.router.navigate(['/followers'], { queryParams: { page: 1, order: 'newest' } });
+}
+
+
 
 }
